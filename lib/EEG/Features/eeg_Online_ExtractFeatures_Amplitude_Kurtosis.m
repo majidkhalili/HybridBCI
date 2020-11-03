f64012d5af20a6d54f59ff19de6d54de3e9e4628
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-03 17:43:07 +0100

    Update eeg_Online_ExtractFeatures_Amplitude_Kurtosis.m

--- lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Kurtosis.m
+++ lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Kurtosis.m
@@ -1,4 +1,11 @@
 function [tempFeature] = eeg_Online_ExtractFeatures_Amplitude_Kurtosis(Data, fs, passbandInterval)
+% EEG_ONLINE_EXTRACTFEATURES_AMPLITUDE_AMPLITUDE_KURTOSIS Calculates Kurtosis of the probability distribution
+% INPUTS:
+%   Data              : Channels X Timepoints X Trials; Separated for eeg, emg, eog
+%   fs                : Sampling rate in Hz
+%   passbandInterval  : Boundaries for the bandpass filters (Delta, Theta, Alpha, ...)
+% OUTPUTS:
+%   features  : Value of the correspondant feature.
 
 % Feature calculation
 tempFeature = kurtosis(Data);
