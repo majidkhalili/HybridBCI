c7a4b40dd9a5f41215d654bd0d42abe5947bcd47
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-03 17:32:11 +0100

    Update eeg_Online_ExtractFeatures_Amplitude_Area_under_Curve.m

--- lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Area_under_Curve.m
+++ lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Area_under_Curve.m
@@ -1,4 +1,11 @@
 function [tempFeature] = eeg_Online_ExtractFeatures_Amplitude_Area_under_Curve(Data, fs, passbandInterval)
+% EEG_ONLINE_EXTRACTFEATURES_AMPLITUDE_AREA_UNDER_CURVE Calculates are by numerical integration.
+% INPUTS:
+%   Data              : Channels X Timepoints X Trials; Separated for eeg, emg, eog
+%   fs                : Sampling rate in Hz
+%   passbandInterval  : Boundaries for the bandpass filters (Delta, Theta, Alpha, ...)
+% OUTPUTS:
+%   features  : Value of the correspondant feature.
 
 % Feature calculation
 tempFeature = trapz(Data);
