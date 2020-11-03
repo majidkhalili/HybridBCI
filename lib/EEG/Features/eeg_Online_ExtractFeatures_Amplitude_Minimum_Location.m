62eb12bdb1b91f4aae1df1f54a649ba27631393c
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-03 18:10:20 +0100

    Update eeg_Online_ExtractFeatures_Amplitude_Minimum_Location.m

--- lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Minimum_Location.m
+++ lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Minimum_Location.m
@@ -1,5 +1,12 @@
 function [tempFeature] = eeg_Online_ExtractFeatures_Amplitude_Minimum_Location(Data, fs, passbandInterval)
- 
+% EEG_ONLINE_EXTRACTFEATURES_AMPLITUDE_MINIMUM_LOCATION Calculates the time instant of the minimum amplitude value.
+% INPUTS:
+%   Data              : Channels X Timepoints X Trials; Separated for eeg, emg, eog
+%   fs                : Sampling rate in Hz
+%   passbandInterval  : Boundaries for the bandpass filters (Delta, Theta, Alpha, ...)
+% OUTPUTS:
+%   features  : Value of the correspondant feature.
+
 % Feature calculation
 [minValue, minLoc] = min(Data);
 tempFeature = minLoc/fs; % Converting the datapoint in time location
