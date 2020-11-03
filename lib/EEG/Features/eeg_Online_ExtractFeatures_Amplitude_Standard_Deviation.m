5d76f80dce3780386620d39471d814a763b7639b
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-03 18:12:58 +0100

    Update eeg_Online_ExtractFeatures_Amplitude_Standard_Deviation.m

--- lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Standard_Deviation.m
+++ lib/EEG/Features/eeg_Online_ExtractFeatures_Amplitude_Standard_Deviation.m
@@ -1,5 +1,12 @@
 function [tempFeature] = eeg_Online_ExtractFeatures_Amplitude_Standard_Deviation(Data, fs, passbandInterval)
- 
+% EEG_ONLINE_EXTRACTFEATURES_AMPLITUDE_STANDARD_DEVIATION Calculatesthe standard deviation.
+% INPUTS:
+%   Data              : Channels X Timepoints X Trials; Separated for eeg, emg, eog
+%   fs                : Sampling rate in Hz
+%   passbandInterval  : Boundaries for the bandpass filters (Delta, Theta, Alpha, ...)
+% OUTPUTS:
+%   features  : Value of the correspondant feature.
+
 % Feature calculation
 tempFeature = nanstd(Data);
       
