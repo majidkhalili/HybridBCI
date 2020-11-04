23b0fe4c563c5f24ba8ddd0bc511cd073b6213b7
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 15:18:20 +0100

    Update CLISLAB_EEG_baseline_correction.m

--- lib/EEG/PreProcessing/Functions/CLISLAB_EEG_baseline_correction.m
+++ lib/EEG/PreProcessing/Functions/CLISLAB_EEG_baseline_correction.m
@@ -10,25 +10,20 @@ function [eeg_thinking, eeg_baseline, eog_thinking, eog_baseline, emg_thinking, 
 %   selectSource    :   Logical array with the selected sources. [EEG EOG EMG].
 % OUTPUTS:
 %   eeg_thinking    :   Struct with the filtered EEG thinking data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 %   eeg_baseline    :   Struct with the filtered EEG baseline data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 %   eog_thinking    :   Struct with the filtered EOG thinking data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 %   eog_baseline    :   Struct with the filtered EOG baseline data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 %   emg_thinking    :   Struct with the filtered EMG thinking data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 %   emg_baseline    :   Struct with the filtered EMG baseline data divided in an array for raw data and an array for each selected band. 
-%                       The amount of arrays would depend on user selection. Each arra has a shape as [Channels X Timepoints X Trials].%% Baseline Correction
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 
 for k=1:size(eeg_thinking.wideband,1)
-    %Baseline correcting EEG
-%     eeg_thinking.wideband(k,:,:) = squeeze(eeg_thinking.wideband(k,:,:)) - mean(squeeze(eeg_baseline.wideband(k,:,:)),1);
-%     eeg_thinking.delta(k,:,:) = squeeze(eeg_thinking.delta(k,:,:)) - mean(squeeze(eeg_baseline.delta(k,:,:)),1);
-%     eeg_thinking.theta(k,:,:) = squeeze(eeg_thinking.theta(k,:,:)) - mean(squeeze(eeg_baseline.theta(k,:,:)),1);
-%     eeg_thinking.alpha(k,:,:) = squeeze(eeg_thinking.alpha(k,:,:)) - mean(squeeze(eeg_baseline.alpha(k,:,:)),1);
-%     eeg_thinking.beta(k,:,:) = squeeze(eeg_thinking.beta(k,:,:)) - mean(squeeze(eeg_baseline.beta(k,:,:)),1);
+%Baseline correcting EEG
 %corrected by wu
     eeg_thinking.wideband(k,:,:) = squeeze(eeg_thinking.wideband(k,:,:)) - squeeze(mean(eeg_baseline.wideband(k,:,:),2))';
     eeg_thinking.delta(k,:,:) = squeeze(eeg_thinking.delta(k,:,:)) - squeeze(mean(eeg_baseline.delta(k,:,:),2))';
@@ -40,16 +35,14 @@ end
 %Baseline correcting EOG
 for k=1:size(eog_thinking.filtered,1)
     %corrected by wu
-eog_thinking.filtered(k,:,:) = squeeze(eog_thinking.filtered(k,:,:)) - squeeze(mean(eog_baseline.filtered(k,:,:),2))';
-%     eog_thinking.filtered(k,:,:) = squeeze(eog_thinking.filtered(k,:,:)) - mean(squeeze(eog_baseline.filtered(k,:,:)),1);
+    eog_thinking.filtered(k,:,:) = squeeze(eog_thinking.filtered(k,:,:)) - squeeze(mean(eog_baseline.filtered(k,:,:),2))';
 
 end
 
 %Baseline correcting EMG
 for k=1:size(emg_thinking.filtered,1)
     %corrected by wu
-emg_thinking.filtered(k,:,:) = squeeze(emg_thinking.filtered(k,:,:)) - squeeze(mean(emg_baseline.filtered(k,:,:),2))';
-%     emg_thinking.filtered(k,:,:) = squeeze(emg_thinking.filtered(k,:,:)) - mean(squeeze(emg_baseline.filtered(k,:,:)),1);
+    emg_thinking.filtered(k,:,:) = squeeze(emg_thinking.filtered(k,:,:)) - squeeze(mean(emg_baseline.filtered(k,:,:),2))';
 
 end
 
