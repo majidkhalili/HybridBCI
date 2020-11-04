177248bb09bab1744f28534a27ba482aee0817e2
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 16:38:38 +0100

    Update CLISLAB_NIRS_Online_ExtractFeatures.m

--- lib/NIRS/CLISLAB_NIRS_Online_ExtractFeatures.m
+++ lib/NIRS/CLISLAB_NIRS_Online_ExtractFeatures.m
@@ -1,17 +1,15 @@
 function [nirsFeatures, nirsFeaturesLabels]=CLISLAB_NIRS_Online_ExtractFeatures(data, fs, selectSource, featureList)
-% 
-% thinkingData = struct containing the signals from which extract the
-% features.
-% 
-% channelsLabels = labels of all the channels of the signals in thinkingData
-% 
-% fs = sampling rate
-% 
-% featIndex = index of the features to extract. if missing or empty, 
-% all the possible features are extracted
-% 
-% selectSource = select the source with a 1x3 vector [HbO HbR HbT], 1 for selecting the
-% source, 0 for discarding the source, default [1 1 1]
+% CLISLAB_NIRS_ONLINE_EXTRACTFEATURES Calls to the correspondant NIRS feature functions.
+% INPUTS:
+%   data            :   Struct with the signals from which extract the features. The struct is divided in 3 structs: HbO, HbR and HbT. Some of these structs 
+%                       could not appear depending on user selection.
+%   fs              :   Sampling rate value.
+%   selectSource    :   Logical array with the selected sources. [HbO HbR HbT].
+%   featureList     :   Cell array with the names of the features selected by the user.
+%
+% OUTPUTS:
+%   nirsFeatures        :   Array with the calculated values. [Trials X Features].
+%   nirsFeaturesLabels  :   Array with the label of each row in 'nirsFeatures' (Feature name, band and channel).
 
 HbO = selectSource(1);
 HbR = selectSource(2);
@@ -43,7 +41,6 @@ featureList = unique(featureList(1,:));
 for i = 1:length(featureList)
     featureFunction = str2func(featureList{i});
     feature = featureFunction(data);
-    %[newFeatures, newFeaturesLabels] = feval(string(featureList(i)), thinkingData, sRate);
     
     nirsFeatures = [nirsFeatures feature.features];
     nirsFeaturesLabels = [nirsFeaturesLabels; feature.featuresLabels];
