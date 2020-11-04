d93cc1cd93d9448bb39b7abf85b1fa3fa380cd32
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 17:41:11 +0100

    Update CLISLAB_NIRS_RMS.m

--- lib/NIRS/Features/CLISLAB_NIRS_RMS.m
+++ lib/NIRS/Features/CLISLAB_NIRS_RMS.m
@@ -1,7 +1,18 @@
 function feature = CLISLAB_NIRS_RMS(indata)
-% indata.od = channelXtimepointsXtrials
-% feature.featuresLabels = string;
-% feature.features = trialXfeatures
+% CLISLAB_NIRS_RMS Calculates the root mean square.
+% INPUTS:
+%   indata  :   Struct with the signals from which extract the features. The struct is divided in 6 structs: HbO, HbR, HbT, od, fs and source. 
+%               Some of these structs could not appear depending on user selection. 
+%                 - HbO     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbR     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbT     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - od      : Struct that contains the data after OxyDeoxy conversion with a shape as [(3 X Channels) X Timepoints X Trials].
+%                 - fs      : Sampling rate value.
+%                 - source  : Logical array with the selected sources. [HbO HbR HbT].
+% OUTPUTS:
+%   feature  : Struct with the value of the correspondant feature. The struct is divided in 2 structs: featureLabels and features.
+%               - featureLabels : String array with the name of the feature and the correspondant source with a shape as [(3 X Channels)].
+%               - features      : Array with the feature values with shape [Trials X (3 X Channels)].
 
 labels = "RMS";
 
@@ -13,19 +24,12 @@ numSignal = sum(indata.source);
 
 data = indata.od;
 
-% % Old Version
-% trainfeature=[];
-% 
-% for n = 1:size(data,3)
-%     
-%     trainfeature= [trainfeature; rms(squeeze(data(:,:,n)'))];
-%     
-% end
-
 % New version by Ale
 r = rms(data,2);
 r = squeeze(r);
 feature.featuresLabels = repmat(feature.featuresLabels,size(r,1)/numSignal,1);
 feature.featuresLabels = feature.featuresLabels(:);
 feature.features = r';
+
+end
 
