f01eb7839d0bcf51431b6816f21a4a5595cf5d5c
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 15:39:53 +0100

    Update CLISLAB_EEG_normalization.m

--- lib/EEG/PreProcessing/Functions/CLISLAB_EEG_normalization.m
+++ lib/EEG/PreProcessing/Functions/CLISLAB_EEG_normalization.m
@@ -1,25 +1,33 @@
 function [eeg_thinking, eeg_baseline, eog_thinking, eog_baseline, emg_thinking, emg_baseline] = normalization(eeg_thinking, eeg_baseline, eog_thinking, eog_baseline, emg_thinking, emg_baseline,selectSource)
-%% Normalizing EEG
+% CLISLAB_EEG_NORMALIZATION Calculates the normalization.
+% INPUTS:
+%   eeg_thinking    :   Struct with the EEG thinking data. [Channels X Timepoints X Trials].
+%   eeg_baseline    :   Struct with the EEG baseline data. [Channels X Timepoints X Trials].
+%   eog_thinking    :   Struct with the EOG thinking data. [Channels X Timepoints X Trials].
+%   eog_baseline    :   Struct with the EOG baseline data. [Channels X Timepoints X Trials].
+%   emg_thinking    :   Struct with the EMG thinking data. [Channels X Timepoints X Trials].
+%   emg_baseline    :   Struct with the EMG baseline data. [Channels X Timepoints X Trials].
+%   selectSource    :   Logical array with the selected sources. [EEG EOG EMG].
+% OUTPUTS:
+%   eeg_thinking    :   Struct with the filtered EEG thinking data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
+%   eeg_baseline    :   Struct with the filtered EEG baseline data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
+%   eog_thinking    :   Struct with the filtered EOG thinking data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
+%   eog_baseline    :   Struct with the filtered EOG baseline data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
+%   emg_thinking    :   Struct with the filtered EMG thinking data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
+%   emg_baseline    :   Struct with the filtered EMG baseline data divided in an array for raw data and an array for each selected band. 
+%                       The amount of arrays would depend on user selection. Each array has a shape as [Channels X Timepoints X Trials].
 for k=1:size(eeg_thinking.wideband,1)
     % Modified AJG 2.08.2019
-%     eeg_thinking.wideband(k,:,:)   =   squeeze(eeg_thinking.wideband(k,:,:)) - mean(squeeze(eeg_thinking.wideband(k,:,:)),1);
-%     eeg_thinking.delta(k,:,:)      =   squeeze(eeg_thinking.delta(k,:,:)) - mean(squeeze(eeg_thinking.delta(k,:,:)),1);
-%     eeg_thinking.theta(k,:,:)      =   squeeze(eeg_thinking.theta(k,:,:)) - mean(squeeze(eeg_thinking.theta(k,:,:)),1);
-%     eeg_thinking.alpha(k,:,:)      =   squeeze(eeg_thinking.alpha(k,:,:)) - mean(squeeze(eeg_thinking.alpha(k,:,:)),1);
-%     eeg_thinking.beta(k,:,:)       =   squeeze(eeg_thinking.beta(k,:,:)) - mean(squeeze(eeg_thinking.beta(k,:,:)),1);
-
     eeg_thinking.wideband(k,:,:) = squeeze(eeg_thinking.wideband(k,:,:)) - squeeze(mean(eeg_thinking.wideband(k,:,:),2))';
     eeg_thinking.delta(k,:,:) = squeeze(eeg_thinking.delta(k,:,:)) - squeeze(mean(eeg_thinking.delta(k,:,:),2))';
     eeg_thinking.theta(k,:,:) = squeeze(eeg_thinking.theta(k,:,:)) - squeeze(mean(eeg_thinking.theta(k,:,:),2))';
     eeg_thinking.alpha(k,:,:) = squeeze(eeg_thinking.alpha(k,:,:)) - squeeze(mean(eeg_thinking.alpha(k,:,:),2))';
     eeg_thinking.beta(k,:,:) = squeeze(eeg_thinking.beta(k,:,:)) - squeeze(mean(eeg_thinking.beta(k,:,:),2))';
-
-        % Modified AJG 2.08.2019
-%     eeg_baseline.wideband(k,:,:)   =   squeeze(eeg_baseline.wideband(k,:,:)) - mean(squeeze(eeg_baseline.wideband(k,:,:)),1);
-%     eeg_baseline.delta(k,:,:)      =   squeeze(eeg_baseline.delta(k,:,:)) - mean(squeeze(eeg_baseline.delta(k,:,:)),1);
-%     eeg_baseline.theta(k,:,:)      =   squeeze(eeg_baseline.theta(k,:,:)) - mean(squeeze(eeg_baseline.theta(k,:,:)),1);
-%     eeg_baseline.alpha(k,:,:)      =   squeeze(eeg_baseline.alpha(k,:,:)) - mean(squeeze(eeg_baseline.alpha(k,:,:)),1);
-%     eeg_baseline.beta(k,:,:)      =   squeeze(eeg_baseline.beta(k,:,:)) - mean(squeeze(eeg_baseline.beta(k,:,:)),1);
 
     eeg_baseline.wideband(k,:,:) = squeeze(eeg_baseline.wideband(k,:,:)) - squeeze(mean(eeg_baseline.wideband(k,:,:),2))';
     eeg_baseline.delta(k,:,:) = squeeze(eeg_baseline.delta(k,:,:)) - squeeze(mean(eeg_baseline.delta(k,:,:),2))';
@@ -34,8 +42,6 @@ for k=1:size(eog_thinking.filtered,1)
     eog_thinking.filtered(k,:,:)   =   squeeze(eog_thinking.filtered(k,:,:)) - squeeze(mean(eog_thinking.filtered(k,:,:),2))';
     eog_baseline.filtered(k,:,:)   =   squeeze(eog_baseline.filtered(k,:,:)) - squeeze(mean(eog_baseline.filtered(k,:,:),2))';
  
-%     eog_thinking.filtered(k,:,:)   =   squeeze(eog_thinking.filtered(k,:,:)) - mean(squeeze(eog_thinking.filtered(k,:,:)),1);
-%     eog_baseline.filtered(k,:,:)   =   squeeze(eog_baseline.filtered(k,:,:)) - mean(squeeze(eog_baseline.filtered(k,:,:)),1);
 end
 
 %Normalizing EMG
@@ -44,8 +50,6 @@ for k=1:size(emg_thinking.filtered,1)
      emg_thinking.filtered(k,:,:)   =   squeeze(emg_thinking.filtered(k,:,:)) - squeeze(mean(emg_thinking.filtered(k,:,:),2))';
      emg_baseline.filtered(k,:,:)   =   squeeze(emg_baseline.filtered(k,:,:)) - squeeze(mean(emg_baseline.filtered(k,:,:),2))';
        
-%     emg_thinking.filtered(k,:,:)   =   squeeze(emg_thinking.filtered(k,:,:)) - mean(squeeze(emg_thinking.filtered(k,:,:)),1);
-%     emg_baseline.filtered(k,:,:)   =   squeeze(emg_baseline.filtered(k,:,:)) - mean(squeeze(emg_baseline.filtered(k,:,:)),1);
 end
 
 end
