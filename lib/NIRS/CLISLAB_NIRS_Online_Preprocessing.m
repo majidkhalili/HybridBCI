e7e57a6fbf9e002d82974f2c1a4b86aa28e28f92
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 17:13:02 +0100

    Update CLISLAB_NIRS_Online_Preprocessing.m

--- lib/NIRS/CLISLAB_NIRS_Online_Preprocessing.m
+++ lib/NIRS/CLISLAB_NIRS_Online_Preprocessing.m
@@ -1,12 +1,25 @@
 function [baseline, preproc_thinking] = CLISLAB_NIRS_Online_Preprocessing(wl_baseline, wl_thinking, fs, conversion_type, baseline_reference, filter_type, filter_cut_freqs, interprocess)
+% CLISLAB_NIRS_ONLINE_PREPROCESSING Calls to the correspondant NIRS preprocessing functions.
+% INPUTS:
+%   wl_baseline         :   Array containing the baseline data. [Channels X Timepoints X Trials].
+%   wl_thinking         :   Array containing the thinking data. [Channels X Timepoints X Trials].
+%   fs                  :   Sampling rate value.
+%   conversion_type     :   String with the name of the conversion function to be applied. Selected by user.
+%   baseline_reference  :   String with the name of the baseline reference selection. Selected by user.
+%   filter_type         :   String with the name of the filter to be applied. Selected by user.
+%   filter_cut_freqs    :   Array with filter range (lower and upper values). Selected by user.
+%   interprocess        :   Cell array containing the name of the processing functions to be applied. Selected by user.
+%
+% OUTPUTS:
+%   baseline            : Struct containing the processed baseline data. The struct is divided in 3 structs: HbO, HbR and HbT. Some of these structs 
+%                         could not appear depending on user selection. Each struct has the shape as [Channels X Timepoints X Trials].
+%   preproc_thinking    : Struct containing the processed thinking data. The struct is divided in 3 structs: HbO, HbR and HbT. Some of these structs 
+%                         could not appear depending on user selection. Each struct has the shape as [Channels X Timepoints X Trials].
 
 oxydeoxyConversion = str2func(conversion_type);
 filterFunction = str2func(filter_type);
-%baselineCorrection = str2func(baseline_reference);
 
 [od_thinking, od_baseline] = oxydeoxyConversion(wl_thinking, wl_baseline,fs, baseline_reference);
-
-%baselineCorr_thinking = baselineCorrection(od_thinking,od_baseline);
 
 baseline.hbo = od_baseline(1:end/2,:,:);
 baseline.hbr = od_baseline(end/2+1:end,:,:);
@@ -31,3 +44,5 @@ end
 
 baseline = interprocess_baseline;
 preproc_thinking = interprocess_thinking;
+
+end
