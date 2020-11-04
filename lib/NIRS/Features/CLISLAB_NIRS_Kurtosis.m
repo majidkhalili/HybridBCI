472c3e8e974e4795bcd5b4e2a3ab9fdf27fefc53
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 17:33:28 +0100

    Update CLISLAB_NIRS_Kurtosis.m

--- lib/NIRS/Features/CLISLAB_NIRS_Kurtosis.m
+++ lib/NIRS/Features/CLISLAB_NIRS_Kurtosis.m
@@ -1,7 +1,18 @@
 function feature = CLISLAB_NIRS_Kurtosis(indata)
-% indata.od = channelXtimepointsXtrials
-% feature.featuresLabels = string;
-% feature.features = trialXfeaturesname = 'kurtosis';
+% CLISLAB_NIRS_KURTOSIS Calculates the index of gaussianity of the data distribution.
+% INPUTS:
+%   indata  :   Struct with the signals from which extract the features. The struct is divided in 6 structs: HbO, HbR, HbT, od, fs and source. 
+%               Some of these structs could not appear depending on user selection. 
+%                 - HbO     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbR     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbT     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - od      : Struct that contains the data after OxyDeoxy conversion with a shape as [(3 X Channels) X Timepoints X Trials].
+%                 - fs      : Sampling rate value.
+%                 - source  : Logical array with the selected sources. [HbO HbR HbT].
+% OUTPUTS:
+%   feature  : Struct with the value of the correspondant feature. The struct is divided in 2 structs: featureLabels and features.
+%               - featureLabels : String array with the name of the feature and the correspondant source with a shape as [(3 X Channels)].
+%               - features      : Array with the feature values with shape [Trials X (3 X Channels)].
 
 labels = "Kurtosis";
 
@@ -13,20 +24,11 @@ numSignal = sum(indata.source);
 
 data = indata.od;
 
-%    using kurtosis values of each channel as feature
-
-% % Old Version
-% trainfeature=[];
-% 
-% for n = 1:size(data,3)
-%     
-%     trainfeature= [trainfeature; kurtosis(squeeze(data(:,:,n)'))];
-%     
-% end
-
 % New Version by Ale
 k = kurtosis(data,1,2);
 k = squeeze(k);
 feature.featuresLabels = repmat(feature.featuresLabels,size(k,1)/numSignal,1);
 feature.featuresLabels = feature.featuresLabels(:);
 feature.features = k';
+
+end
