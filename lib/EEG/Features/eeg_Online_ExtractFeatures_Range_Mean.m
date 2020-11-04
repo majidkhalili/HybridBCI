16ee18ba088f9c16041f198111bd7d5c509fabc1
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 09:35:45 +0100

    Update eeg_Online_ExtractFeatures_Range_Mean.m

--- lib/EEG/Features/eeg_Online_ExtractFeatures_Range_Mean.m
+++ lib/EEG/Features/eeg_Online_ExtractFeatures_Range_Mean.m
@@ -1,4 +1,11 @@
 function [tempFeature] = eeg_Online_ExtractFeatures_Range_Mean(Data, fs, passbandInterval)
+% EEG_ONLINE_EXTRACTFEATURES_RANGE_MEAN Calculates the mean value of the range EEG.
+% INPUTS:
+%   Data              : Channels X Timepoints X Trials; Separated for eeg, emg, eog
+%   fs                : Sampling rate in Hz
+%   passbandInterval  : Boundaries for the bandpass filters (Delta, Theta, Alpha, ...)
+% OUTPUTS:
+%   tempFeature  : Value of the correspondant feature.
 
 % Parameters for generating ranges
 windowLength	=   (size(Data,2)/fs)/8; %Half the length of the time
