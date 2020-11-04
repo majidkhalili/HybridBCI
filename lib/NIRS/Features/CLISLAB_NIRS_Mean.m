6630abd8d50093021c0bcdbb8a15de2ac170e855
JayroMartinez <43171040+JayroMartinez@users.noreply.github.com>
2020-11-04 17:39:42 +0100

    Update CLISLAB_NIRS_Mean.m

--- lib/NIRS/Features/CLISLAB_NIRS_Mean.m
+++ lib/NIRS/Features/CLISLAB_NIRS_Mean.m
@@ -1,7 +1,19 @@
 function feature = CLISLAB_NIRS_Mean(indata)
-% indata.od = channelXtimepointsXtrials
-% feature.featuresLabels = string;
-% feature.features = trialXfeatures
+
+% CLISLAB_NIRS_MEAN Calculates the imean of the signal.
+% INPUTS:
+%   indata  :   Struct with the signals from which extract the features. The struct is divided in 6 structs: HbO, HbR, HbT, od, fs and source. 
+%               Some of these structs could not appear depending on user selection. 
+%                 - HbO     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbR     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - HbT     : Struct with a shape as [Channels X Timepoints X Trials]. 
+%                 - od      : Struct that contains the data after OxyDeoxy conversion with a shape as [(3 X Channels) X Timepoints X Trials].
+%                 - fs      : Sampling rate value.
+%                 - source  : Logical array with the selected sources. [HbO HbR HbT].
+% OUTPUTS:
+%   feature  : Struct with the value of the correspondant feature. The struct is divided in 2 structs: featureLabels and features.
+%               - featureLabels : String array with the name of the feature and the correspondant source with a shape as [(3 X Channels)].
+%               - features      : Array with the feature values with shape [Trials X (3 X Channels)].
 
 labels = "Mean";
 
@@ -13,18 +25,11 @@ numSignal = sum(indata.source);
 
 data = indata.od;
 
-% % OLD VERSION
-% trainfeature=[];
-% 
-% for n = 1:size(rdata,3)
-%     
-%     trainfeature= [trainfeature; mean((squeeze(rdata(:,:,n)')))];
-%     
-% end
-
 % NEW Version by Ale
 m = mean(data,2);
 m = squeeze(m);
 feature.featuresLabels = repmat(feature.featuresLabels,size(m,1)/numSignal,1);
 feature.featuresLabels = feature.featuresLabels(:);
 feature.features = m';
+
+end
